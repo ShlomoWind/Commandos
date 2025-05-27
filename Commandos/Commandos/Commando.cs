08ec1a9c9c6916c8011f236e1c807c68f40ed73b
Shlomo <sh.z.wind@gmail.com>
2025-05-27 10:26:53 +0300

    added all functions of commando

--- Commandos/Commandos/Commando.cs
+++ Commandos/Commandos/Commando.cs
@@ -1,15 +1,33 @@
-﻿public class Commando
+﻿using System;
+
+public class Commando
 {
     public string Name;
     public string CodeName;
-    public string[] Tools;
+    public string[] Tools = new string[5];
     public string Status;
 
-    public Commando(string name, string codeName, string[] tools, string status)
+    public Commando(string name, string codeName)
     {
+        string[] tools = { "hammer", "chisel", "rope", "bag", "water bottle" };
         this.Name = name;
         this.CodeName = codeName;
         this.Tools = tools;
-        this.Status = status;
+        this.Status = "standing";
+    }
+
+    public void Walk()
+    {
+        this.Status = "walking";
+        Console.WriteLine("soldier is walking");
+    }
+    public void Hide()
+    {
+        this.Status = "hiding";
+        Console.WriteLine("soldier is hid");
+    }
+    public void Attack()
+    {
+        Console.WriteLine($"soldier {this.CodeName} is attacking");
     }
 }
