d5296416d9935c2bacb2e48e936430c588453b5f
Shlomo <sh.z.wind@gmail.com>
2025-05-27 10:09:50 +0300

    create class Commando and constractor

---
+++ Commandos/Commandos/Commando.cs
@@ -0,0 +1,15 @@
+﻿public class Commando
+{
+    public string Name;
+    public string CodeName;
+    public string[] Tools;
+    public string Status;
+
+    public Commando(string name, string codeName, string[] tools, string status)
+    {
+        this.Name = name;
+        this.CodeName = codeName;
+        this.Tools = tools;
+        this.Status = status;
+    }
+}
